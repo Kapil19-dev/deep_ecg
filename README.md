e08a042d460c9cafdc96140df45e75f437342f8e
lingfliu <lingf.liu@gmail.com>
2019-10-30 22:56:23 +0800

    repo inited

---
+++ README.md
@@ -0,0 +1,6 @@
+# Deep ECG
+Deep learning for ECG denoising, feature detection, and classification
+
+## Introduction
+
+## Usage demos
---
+++ sigtool/linear_filter.py
@@ -0,0 +1,11 @@
+def fir(b, a, s):
+    pass
+
+def iir(b, s):
+    pass
+
+
+'''the following parameters are provided for signals of sps 500 Hz'''
+fir_lp_45_param = []
+
+fir_lp_25_param = []
